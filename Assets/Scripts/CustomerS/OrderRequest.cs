5dd8b92b9ae3fce1891fc91fd0a884ed159f4054
Y4IR0 <yairo_k@proton.me>
2025-01-16 15:14:55 +0100

    fixed OrderRequest OrderCheck() method

--- Assets/Scripts/CustomerS/OrderRequest.cs
+++ Assets/Scripts/CustomerS/OrderRequest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class OrderRequest : MonoBehaviour
@@ -12,8 +13,10 @@ public class OrderRequest : MonoBehaviour
         
         if (GameManager.instance.playerCarry)
             if (GameManager.instance.playerCarry.carryingObject)
-                if (GameManager.instance.playerCarry.carryingObject.TryGetComponent<CompletedOrder>(out CompletedOrder component))
-                    completedOrder = component;
+                if (GameManager.instance.playerCarry.carryingObject.TryGetComponent<Plate>(out Plate plate))
+                    if (GameManager.instance.playerCarry.carryingObject.transform.GetChild(0))
+                        if (GameManager.instance.playerCarry.carryingObject.transform.GetChild(0).TryGetComponent<CompletedOrder>(out CompletedOrder component))
+                            completedOrder = component;
 
         return completedOrder;
     }
@@ -48,7 +51,20 @@ public class OrderRequest : MonoBehaviour
             return;
         }
 
-        if (completedOrder.name == order.name) // Currently checks by name, change when needed.
+        /*
+        Debug.Log(completedOrder);
+        string prefabPath0 = AssetDatabase.GetAssetPath(completedOrder);
+        string prefabPath1 = AssetDatabase.GetAssetPath(order.gameObject);
+
+        Debug.Log(prefabPath0);
+        Debug.Log(prefabPath1);
+
+        if (prefabPath0 == prefabPath1) // Checks if they have the same prefab
+            isCorrectCompletedOrder = true;
+        */
+
+
+        if (completedOrder.sprite == order.sprite) // Checks if sprites are the same, if they are it uses the same prefab
             isCorrectCompletedOrder = true;
 
         if (isCorrectCompletedOrder)
@@ -62,3 +78,20 @@ public class OrderRequest : MonoBehaviour
         CheckOrder();
     }
 }
+
+
+/*
+                Component[] components = child.GetComponents<Component>();
+                System.Type baseType = typeof(Ingredient);
+
+                foreach (Component component in components)
+                {
+                    if (component == null)
+                        continue;
+                    
+                    if (component.GetType().IsSubclassOf(baseType))
+                    {
+                        Debug.Log(component.GetType().Name);
+                    }
+                }
+                */
