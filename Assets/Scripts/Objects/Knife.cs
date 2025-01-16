171055f7afcf47ac16a2cf66578c540e2dad2484
File_Missing <michaverheij@gmail.com>
2025-01-16 11:54:22 +0100

    fixed knife pickup bug

--- Assets/Scripts/Objects/Knife.cs
+++ Assets/Scripts/Objects/Knife.cs
@@ -1,8 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
 
 public class Knife : Carryable
 {
-
+    public override void InteractLeft()
+    {
+        base.InteractLeft();
+        if(transform.parent && transform.parent.TryGetComponent<KnifeHolder>(out KnifeHolder holder))
+        {
+            holder.knife = null;
+        }
+    }
 }
