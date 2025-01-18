18e73b0c8e0a6bdca8dd303693064f29846dbd91
File_Missing <michaverheij@gmail.com>
2025-01-18 15:40:17 +0100

    added a simple cursor changing function for interaction

--- Assets/Scripts/Player/Interact.cs
+++ Assets/Scripts/Player/Interact.cs
@@ -1,19 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
+[System.Serializable]
 public class Interact : MonoBehaviour
 {
     public bool canInteract;
     public float interactionRange;
     public LayerMask interactionMask;
-    RaycastHit hit;
+    private RaycastHit hit;
+
+    [Header("Cursor")]
+    public Image cursorImage;
+    public Sprite defaultSprite;
+    public Sprite highlightSprite;
     
     void Update()
     {
         if (!canInteract) return;
 
+        ChangeCursor();
+
+        SendMessages();
+    }
+
+    void ChangeCursor()
+    {
+        bool highlight = Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, interactionRange, interactionMask, QueryTriggerInteraction.Collide);
+        cursorImage.sprite = highlight ? highlightSprite : defaultSprite;
+    }
+    
+    void SendMessages()
+    {
         if (Input.GetMouseButtonDown(0))// left mouse button
         {
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactionRange, interactionMask, QueryTriggerInteraction.Collide))
@@ -29,5 +48,5 @@ public class Interact : MonoBehaviour
                 hit.transform.SendMessage("InteractRight", SendMessageOptions.DontRequireReceiver);
             }
         }
-    }
+    }    
 }
