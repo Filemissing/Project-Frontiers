659b09157c34c0ad2d51ddb3af6c88c02a50eaac
File_Missing <michaverheij@gmail.com>
2025-01-22 16:45:11 +0100

    added outline package and methods to carryable component, works only on the knife for some reason?

--- Assets/Scripts/Actions/Carryable.cs
+++ Assets/Scripts/Actions/Carryable.cs
@@ -2,8 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Outline))]
 public class Carryable : MonoBehaviour
 {
+    Outline outline;
+    int lastOutlineFrame;
+
+    public virtual void Awake()
+    {
+        if (!TryGetComponent<Outline>(out outline)) Debug.Log("Could not get Outline component");
+        else Debug.Log(outline);
+    }
+
+    public virtual void Outline()
+    {
+        outline.enabled = true;
+        lastOutlineFrame = Time.frameCount;
+    }
+
+
+    public void Update()
+    {
+        if(Time.frameCount > lastOutlineFrame)
+        {
+            outline.enabled = false;
+        }
+    }
+
     public virtual void InteractLeft()
     {
         if(GameManager.instance.playerCarry.carryingObject == null)
