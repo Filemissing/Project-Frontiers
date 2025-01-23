819b6e82a2d7c2857f453b38d69f04141f4fcfb2
Y4IR0 <yairo_k@proton.me>
2025-01-23 14:44:44 +0100

    RecipePopup before i it wont make sense anymore
    Due to unity UI prefabs being very buggy, I'm gonna have to take drastic measures to make the RecipePopup work as intended.

---
+++ Assets/Scripts/UI/RecipePopup.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipePopup : MonoBehaviour
+{
+    [SerializeField] Plate plate;
+    [SerializeField] GameObject recipePanel;
+    [SerializeField] Transform recipePanelParent;
+    [SerializeField] GameObject ingredientPanel;
+    [SerializeField] GameObject conditionTextLabel;
+
+    [Header("TESTING")]
+    [SerializeField] Recipe testRecipe;
+
+
+    Sprite GetCompletedRecipeSprite(Recipe recipe)
+    {
+        return recipe.endResult.GetComponent<CompletedOrder>().sprite;
+    }
+
+    void CreateCondition(string conditionString, Transform conditionsTextLabelParent)
+    {
+        GameObject newConditionTextLabel = Instantiate<GameObject>(conditionTextLabel);
+        newConditionTextLabel.transform.SetParent(conditionsTextLabelParent);
+        newConditionTextLabel.GetComponent<TMP_Text>().text = "- " + conditionString;
+    }
+
+    void CreateIngredientPanel(IngredientRequirements ingredientRequirements, Transform ingredientPanelParent)
+    {
+        GameObject newIngredientPanel = Instantiate<GameObject>(ingredientPanel);
+        newIngredientPanel.transform.SetParent(ingredientPanelParent);
+
+        Image ingredientImageLabel = newIngredientPanel.transform.GetChild(0).GetComponent<Image>();
+        ingredientImageLabel.sprite = ingredientRequirements.ingredient.icon;
+
+        TMP_Text ingredientTextLabel = newIngredientPanel.transform.GetChild(1).GetComponent<TMP_Text>();
+        ingredientTextLabel.text = ingredientRequirements.ingredient.name;
+
+        Transform conditionsTextLabelParent = newIngredientPanel.transform.GetChild(2);
+
+        if (ingredientRequirements.isChopped)
+            CreateCondition("Chopped", conditionsTextLabelParent);
+
+        if (ingredientRequirements.isFried)
+            CreateCondition("Fried", conditionsTextLabelParent);
+
+        if (ingredientRequirements.isCooked)
+            CreateCondition("Cooked", conditionsTextLabelParent);
+
+        if (ingredientRequirements.isBurnt)
+            CreateCondition("Burnt", conditionsTextLabelParent);
+    }
+
+    void CreateRecipePanel(Recipe recipe)
+    {
+        GameObject newRecipePanel = Instantiate<GameObject>(recipePanel);
+        newRecipePanel.transform.SetParent(recipePanelParent);
+
+        RectTransform newRecipePanelRectTransform = newRecipePanel.GetComponent<RectTransform>(); // This is added cuz unity UI prefabs buggy
+        newRecipePanelRectTransform.localScale = new Vector3(1, 1, 1);
+        newRecipePanelRectTransform.position = new Vector3(0, -1, 0);
+        newRecipePanelRectTransform.rotation = quaternion.identity;
+        
+        Image completedRecipeImageLabel = newRecipePanel.transform.GetChild(0).GetComponent<Image>();
+        completedRecipeImageLabel.sprite = GetCompletedRecipeSprite(recipe);
+
+        Transform ingredientPanelParent = newRecipePanel.transform.GetChild(1);
+
+        for (int i = 0; i < recipe.ingredients.Length; i++)
+        {
+            IngredientRequirements ingredientRequirements = recipe.ingredients[i];
+            CreateIngredientPanel(ingredientRequirements, ingredientPanelParent);
+        }
+    }
+
+    void Awake()
+    {
+        CreateRecipePanel(testRecipe);
+    }
+}
